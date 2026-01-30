33b0ecbba3555f4896618a748e85ac5bb54d45e2
Henrik Köhl <henrik.jan.bergstrom@gmail.com>
2026-01-30 21:39:34 +0100

    Fixed mobile viewport and text being hidden by url bar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { TypewriterText } from "@/components/TypewriterText";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col justify-between pt-4 pb-4 sm:pt-8 sm:pb-8">
+    <main className="min-h-dvh flex flex-col justify-between pt-4 pb-4 sm:pt-8 sm:pb-8">
       <h1 className="text-[15vw] leading-[0.8] font-normal tracking-tighter w-full text-left pl-2">
         kohl.design
       </h1>
